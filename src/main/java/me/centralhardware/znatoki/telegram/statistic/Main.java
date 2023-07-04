21df9102d32d256dd87b92caabef17043d1b7370
Alexey Fedechkin <alex@centralhardware.me>
2023-07-04 19:38:52 +0200

    fix bot doesn't respond

--- src/main/java/me/centralhardware/znatoki/telegram/statistic/Main.java
+++ src/main/java/me/centralhardware/znatoki/telegram/statistic/Main.java
@@ -2,10 +2,13 @@ package me.centralhardware.znatoki.telegram.statistic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.telegram.telegrambots.starter.TelegramBotStarterConfiguration;
 
 @SpringBootApplication
 @EnableScheduling
+@Import(TelegramBotStarterConfiguration.class)
 public class Main {
     public static void main(String[] args) {
         System.setProperty("org.springframework.boot.logging.LoggingSystem", "none");
