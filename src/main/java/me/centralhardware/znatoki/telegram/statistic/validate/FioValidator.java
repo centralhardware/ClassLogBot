19cf4999418ff7923ba7adc2cabdafeec658e041
Alexey Fedechkin <alex@centralhardware.me>
2023-09-08 07:57:11 +0200

    Revert "add demo"
    This reverts commit b548d322b8371b48f01bbe6214bfe99345cb47a2.

--- src/main/java/me/centralhardware/znatoki/telegram/statistic/validate/FioValidator.java
+++ src/main/java/me/centralhardware/znatoki/telegram/statistic/validate/FioValidator.java
@@ -2,7 +2,6 @@ package me.centralhardware.znatoki.telegram.statistic.validate;
 
 import io.vavr.control.Either;
 import lombok.RequiredArgsConstructor;
-import me.centralhardware.znatoki.telegram.statistic.Configuration;
 import me.centralhardware.znatoki.telegram.statistic.mapper.PupilMapper;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +10,9 @@ import org.springframework.stereotype.Component;
 public class FioValidator implements Validator<String, String> {
 
     private final PupilMapper pupilMapper;
-    private final Configuration configuration;
 
     @Override
     public Either<String, String> validate(String value) {
-        if (configuration.getIsDemo()) return Either.right(value);
-
         if (!pupilMapper.exist(value)){
             return Either.left("ФИО не найдено");
         }
