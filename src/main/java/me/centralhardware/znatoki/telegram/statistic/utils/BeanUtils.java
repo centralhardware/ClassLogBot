6997c9988176d6c94157d80694d6b14ffdbf4313
Alexey Fedechkin <alex@centralhardware.me>
2023-10-21 19:33:15 +0200

    fix bean utils

--- src/main/java/me/centralhardware/znatoki/telegram/statistic/utils/BeanUtils.java
+++ src/main/java/me/centralhardware/znatoki/telegram/statistic/utils/BeanUtils.java
@@ -1,12 +1,18 @@
 package me.centralhardware.znatoki.telegram.statistic.utils;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class BeanUtils {
+public class BeanUtils implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext ac) {
+        BeanUtils.applicationContext = ac;
+    }
 
     public static <T> T getBean(Class<T> clazz){
         return applicationContext.getBean(clazz);
