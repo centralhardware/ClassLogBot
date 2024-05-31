1d675eb30e14a0c07f2125496a43d159bf16b4d5
Fedechkin Aleksei <alex@centralhardware.me>
2024-05-31 09:27:37 +0200

    fix build

--- Dockerfile
+++ Dockerfile
@@ -2,18 +2,16 @@ FROM gradle:jdk21 as gradle
 
 COPY ./ ./
 
-RUN gradle bootJar
+RUN gradle fatJar
 
 FROM openjdk:21-slim
 
 WORKDIR /znatokiBot
 
-COPY --from=gradle /home/gradle/build/libs/znatokiStatistic-1.0-SNAPSHOT.jar .
+COPY --from=gradle /home/gradle/build/libs/znatokiStatistic-1.0-SNAPSHOT-standalone.jar .
 
-RUN apt-get update -y && \
-    apt-get upgrade -y && \
-    apt-get install tzdata curl fontconfig libfreetype6 -y
+RUN apt-get install tzdata curl fontconfig libfreetype6 -y
 
 ENV TZ Asia/Novosibirsk
 
-CMD ["java", "-jar", "znatokiStatistic-1.0-SNAPSHOT.jar" ]
+CMD ["java", "-jar", "znatokiStatistic-1.0-SNAPSHOT-standalone.jar" ]
--- build.gradle.kts
+++ build.gradle.kts
@@ -43,3 +43,20 @@ dependencies {
     testImplementation("org.mockito:mockito-core:5.12.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
 }
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
+        archiveClassifier.set("standalone") // Naming the jar
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest { attributes(mapOf("Main-Class" to "MainKt")) } // Provided we set it up in the application plugin configuration
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+    build {
+        dependsOn(fatJar) // Trigger fat jar creation during build
+    }
+}
