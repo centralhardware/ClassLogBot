8e44a0cb6dbc532cd1562d098b4e4d1905fadff6
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-15 00:19:46 +0200

    bump commons

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("io.github.nsk90:kstatemachine-coroutines:$kstatemachineVersion")
 
     implementation("dev.inmo:tgbotapi:18.2.1")
-    implementation("com.github.centralhardware:telegram-bot-commons:3a0ec97777")
+    implementation("com.github.centralhardware:telegram-bot-commons:1a94263190")
 
     implementation("io.minio:minio:8.5.12")
 
