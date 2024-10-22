c08a5d1613eafd877bba343e42140d7e25da77ac
Fedechkin Aleksei <alex@centralhardware.me>
2024-10-22 21:09:16 +0200

    enable java vector api

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
     implementation("io.github.nsk90:kstatemachine-coroutines:$kstatemachineVersion")
 
     implementation("dev.inmo:tgbotapi:18.2.2")
-    implementation("com.github.centralhardware:telegram-bot-commons:cc1472911c")
+    implementation("com.github.centralhardware:telegram-bot-commons:1b5b37063e")
 
     implementation("io.minio:minio:8.5.12")
 
