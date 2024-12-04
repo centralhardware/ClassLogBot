7c93ae8c5257340d22f6cd6ee9add2ba1786cab8
Fedechkin Aleksei <alex@centralhardware.me>
2024-12-04 06:04:04 +0100

    bump microutils

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
     maven("https://jitpack.io")
+    maven("https://nexus.inmo.dev/repository/maven-releases/")
 }
 
 var poiVersion = "5.3.0"
@@ -30,6 +31,7 @@ dependencies {
     implementation("io.github.nsk90:kstatemachine:$kstatemachineVersion")
     implementation("io.github.nsk90:kstatemachine-coroutines:$kstatemachineVersion")
 
+    implementation("dev.inmo:micro_utils.common:0.23.1")
     implementation("dev.inmo:tgbotapi:20.0.1")
     implementation("com.github.centralhardware:telegram-bot-commons:d96b131958")
 
